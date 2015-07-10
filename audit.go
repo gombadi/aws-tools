7f1688053fbca0d9e78566350cbb0de25471a5c9
Lyndsay Roger <gombadi@users.noreply.github.com>
2015-07-11 09:19:33 +1200

    Add audit of IAM user password & Accesskey last used

--- audit.go
+++ audit.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/mitchellh/cli"
 )
 
@@ -14,6 +16,7 @@ type AuditCommand struct {
 	csv        bool
 	all        bool
 	public_ami bool
+	users      bool
 	Ui         cli.Ui
 }
 
@@ -30,6 +33,7 @@ func (c *AuditCommand) Help() string {
 	--csv - produce output in csv format if possible
 	--all - run all the audit checks
 	--public_ami - check for AMI's owned by account but with public visibility
+	--users - show password & access key last used details
 	`
 }
 
@@ -46,12 +50,17 @@ func (c *AuditCommand) Run(args []string) int {
 	cmdFlags.BoolVar(&c.csv, "csv", false, "Produce output in csv format")
 	cmdFlags.BoolVar(&c.all, "all", false, "Select all Audit options")
 	cmdFlags.BoolVar(&c.public_ami, "public_ami", false, "Audit AMI's for public launch permissions")
+	cmdFlags.BoolVar(&c.users, "users", false, "Audit Users password & AccessKey last used")
 	if err := cmdFlags.Parse(args); err != nil {
 		return RCERR
 	}
 
 	if c.public_ami == true || c.all == true {
 		public_ami(c.verbose, c.csv)
+	}
+
+	if c.users == true || c.all == true {
+		users(c.verbose, c.csv)
 	}
 
 	return RCOK
@@ -66,7 +75,7 @@ func public_ami(verbose bool, csv bool) {
 
 	// Create an EC2 service object
 	// config values keys, sercet key & region read from environment
-	svc := ec2.New(&aws.Config{})
+	svc := ec2.New(&aws.Config{MaxRetries: 10})
 
 	ec2dii := ec2.DescribeImagesInput{Owners: []*string{aws.String("self")}}
 
@@ -90,6 +99,85 @@ func public_ami(verbose bool, csv bool) {
 
 	return
 
+}
+
+// users function will display details on all users and last used info on passwords
+// and access keys
+func users(verbose bool, csv bool) {
+
+	// ListUsers to get a list of all users on the account. Check truncated
+	// Create an IAM service object
+	// Config details Keys, secret keys and region will be read from environment
+	svc := iam.New(&aws.Config{MaxRetries: 10})
+
+	iamlui := &iam.ListUsersInput{
+		Marker:     nil,
+		MaxItems:   nil,
+		PathPrefix: nil,
+	}
+
+	var dowhile bool = true
+
+	// for each user returned record password last used time
+	for dowhile == true {
+
+		iamluo, err := svc.ListUsers(iamlui)
+		if err != nil {
+			if awsErr, ok := err.(awserr.Error); ok {
+				// process SDK error
+				fmt.Printf("AWS Error: %s - %s", awsErr.Code(), awsErr.Message())
+			}
+			return
+		}
+
+		if *iamluo.IsTruncated == false {
+			dowhile = false
+		}
+		iamlui.Marker = iamluo.Marker
+
+		// loop over each user account in IAM
+		for _, user := range iamluo.Users {
+
+			iamlaki := &iam.ListAccessKeysInput{
+				UserName: user.UserName,
+			}
+
+			fmt.Printf("\nUsername: %s\nPassword Last Used: %s\n",
+				*user.UserName,
+				safeDate(user.PasswordLastUsed))
+
+			iamlako, err := svc.ListAccessKeys(iamlaki)
+			if err != nil {
+				fmt.Printf("AWS Error: %s\n", err)
+				return
+			}
+
+			// loop over each access key for the user
+			for _, accesskey := range iamlako.AccessKeyMetadata {
+
+				iamgaklui := &iam.GetAccessKeyLastUsedInput{
+					AccessKeyID: accesskey.AccessKeyID,
+				}
+
+				iamgakluo, err := svc.GetAccessKeyLastUsed(iamgaklui)
+
+				if err != nil {
+					fmt.Printf("AWS Error: %s\n", err)
+					return
+				}
+
+				fmt.Printf("AccessKey: %s\nStatus: %s\nDate Last Used: %s\nRegion: %s\nService: %s\n",
+					*accesskey.AccessKeyID,
+					*accesskey.Status,
+					safeDate(iamgakluo.AccessKeyLastUsed.LastUsedDate),
+					*(chkStringValue(iamgakluo.AccessKeyLastUsed.Region)),
+					*(chkStringValue(iamgakluo.AccessKeyLastUsed.ServiceName)))
+			}
+		}
+
+	}
+
+	return
 }
 
 /*
