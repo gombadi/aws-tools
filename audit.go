48116bef26ff93910b0c4d06f3445af6475da236
Lyndsay Roger <gombadi@users.noreply.github.com>
2015-07-12 13:47:51 +1200

    Add audit snapshots command to display info on snapshots not associated with an AMI

--- audit.go
+++ audit.go
@@ -17,6 +17,7 @@ type AuditCommand struct {
 	all        bool
 	public_ami bool
 	users      bool
+	snapshots  bool
 	Ui         cli.Ui
 }
 
@@ -34,6 +35,7 @@ func (c *AuditCommand) Help() string {
 	--all - run all the audit checks
 	--public_ami - check for AMI's owned by account but with public visibility
 	--users - show password & access key last used details
+	--snapshots - show snapshots that are not associated with an AMI
 	`
 }
 
@@ -51,6 +53,7 @@ func (c *AuditCommand) Run(args []string) int {
 	cmdFlags.BoolVar(&c.all, "all", false, "Select all Audit options")
 	cmdFlags.BoolVar(&c.public_ami, "public_ami", false, "Audit AMI's for public launch permissions")
 	cmdFlags.BoolVar(&c.users, "users", false, "Audit Users password & AccessKey last used")
+	cmdFlags.BoolVar(&c.snapshots, "snapshots", false, "Show snapshots not associated with an AMI")
 	if err := cmdFlags.Parse(args); err != nil {
 		return RCERR
 	}
@@ -61,6 +64,10 @@ func (c *AuditCommand) Run(args []string) int {
 
 	if c.users == true || c.all == true {
 		users(c.verbose, c.csv)
+	}
+
+	if c.snapshots == true || c.all == true {
+		snapshots(c.verbose, c.csv)
 	}
 
 	return RCOK
@@ -178,6 +185,74 @@ func users(verbose bool, csv bool) {
 	}
 
 	return
+}
+
+// snapshots function displays any snapshot that is not associated with an AMI
+func snapshots(verbose bool, csv bool) {
+
+	// Create an EC2 service object
+	// config values keys, sercet key & region read from environment
+	svc := ec2.New(&aws.Config{MaxRetries: 10})
+
+	owners := []*string{aws.String("self")}
+
+	ec2dssi := ec2.DescribeSnapshotsInput{OwnerIDs: owners}
+
+	resp, err := svc.DescribeSnapshots(&ec2dssi)
+
+	if err != nil {
+		fmt.Printf("Fatal error: %s\n", err)
+		return
+	}
+
+	if len(resp.Snapshots) == 0 {
+		fmt.Printf("No Snapshots found for this account\n")
+		return
+	}
+
+	ssm := make(map[string]string, len(resp.Snapshots))
+
+	// fill the map with a key of each snapshot you own
+	for _, ss := range resp.Snapshots {
+		ssm[*ss.SnapshotID] = *ss.Description
+	}
+
+	// Find all the account ami's
+	ec2dii := ec2.DescribeImagesInput{Owners: owners}
+
+	imagesResp, err := svc.DescribeImages(&ec2dii)
+
+	if err != nil {
+		fmt.Printf("Fatal error: %s\n", err)
+		return
+	}
+
+	for _, image := range imagesResp.Images {
+		for _, blockDM := range image.BlockDeviceMappings {
+
+			if blockDM.EBS != nil {
+				// remove this snapshot reference as it is associated with an AMI
+				delete(ssm, *blockDM.EBS.SnapshotID)
+			}
+		}
+
+	}
+
+	if verbose == true {
+		fmt.Printf("\nSnapshots not associated with an AMI owned by this account\n")
+		for k, v := range ssm {
+			fmt.Printf("ID: %s\tDesctiption: %s\n", k, v)
+		}
+
+		fmt.Printf("A total of %v snapshots found\n", len(ssm))
+	} else {
+		for k := range ssm {
+			fmt.Printf("%s\n", k)
+		}
+	}
+
+	return
+
 }
 
 /*
