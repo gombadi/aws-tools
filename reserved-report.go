af3921442cfe8fb6e2f9c30b7c27418569ba0801
Lyndsay Roger <gombadi@users.noreply.github.com>
2015-07-16 10:33:35 +1200

    Add concurrency to reserver-report processing

--- reserved-report.go
+++ reserved-report.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -28,15 +29,15 @@ func (c *RRCommand) Help() string {
 		awsgo-tools reserved-report [flags]
 
 	Flags:
-	-h - print headers and exit
-	-e - produce an empty line if no reserved instances found
 	-a <account name> - account name to use in CSV output
+	-e - produce an empty line if no reserved instances found
+	-h - print headers and exit
 	`
 }
 
 // Synopsis function returns a string with concise details of the sub command
 func (c *RRCommand) Synopsis() string {
-	return "Reserved Instance report CSV Output"
+	return "EC2 & RDS reserved Instance report CSV Output"
 }
 
 // Run function is the function called by the cli library to run the actual sub command code.
@@ -49,6 +50,7 @@ func (c *RRCommand) Run(args []string) int {
 	cmdFlags.BoolVar(&c.printEmpty, "e", false, "Print empty line if no reserved instances found")
 	cmdFlags.StringVar(&c.account, "a", "unknown", "AWS Account Name to use")
 	if err := cmdFlags.Parse(args); err != nil {
+		fmt.Printf("Error processing commandline flags\n")
 		return RCERR
 	}
 
@@ -57,26 +59,58 @@ func (c *RRCommand) Run(args []string) int {
 		return RCOK
 	}
 
-	// Create an EC2 service object
-	// Config details Keys, secret keys and region will be read from environment
-	ec2svc := ec2.New(&aws.Config{MaxRetries: 10})
+	var wg sync.WaitGroup
+	var ec2resp *ec2.DescribeReservedInstancesOutput
+	var rdsresp *rds.DescribeReservedDBInstancesOutput
+	var ec2err, rdserr error
 
-	ec2Filter := ec2.Filter{}
-	ec2Filter.Name = aws.String("state")
-	ec2Filter.Values = []*string{aws.String("active")}
+	// use concurrency to ask AWS multiple questions at once
+	wg.Add(1)
+	go func() {
 
-	ec2drii := ec2.DescribeReservedInstancesInput{Filters: []*ec2.Filter{&ec2Filter}}
+		defer wg.Done()
+		// setup ec2 filter
+		ec2Filter := ec2.Filter{}
+		ec2Filter.Name = aws.String("state")
+		ec2Filter.Values = []*string{aws.String("active")}
+		ec2drii := ec2.DescribeReservedInstancesInput{Filters: []*ec2.Filter{&ec2Filter}}
 
-	// Call the DescribeInstances Operation
-	resp, err := ec2svc.DescribeReservedInstances(&ec2drii)
+		// Create an EC2 service object
+		// Config details Keys, secret keys and region will be read from environment
+		ec2svc := ec2.New(&aws.Config{MaxRetries: 10})
 
-	if err != nil {
-		fmt.Printf("Fatal error: %s\n", err)
+		// Call the DescribeInstances Operation
+		ec2resp, ec2err = ec2svc.DescribeReservedInstances(&ec2drii)
+
+	}()
+
+	wg.Add(1)
+	go func() {
+
+		defer wg.Done()
+		// Config details Keys, secret keys and region will be read from environment
+		rdssvc := rds.New(&aws.Config{MaxRetries: 10})
+
+		// Call the DescribeInstances Operation. Note Filters are not currently supported
+		rdsresp, rdserr := rdssvc.DescribeReservedDBInstances(nil)
+
+	}()
+
+	// wait until both goroutines have completed talking to AWS
+	wg.Wait()
+
+	if ec2err != nil {
+		fmt.Printf("AWS error: %s\n", ec2err)
 		return RCERR
 	}
 
-	// extract the private ip address from the instance struct stored in the reservation
-	for _, ri := range resp.ReservedInstances {
+	if rdserr != nil {
+		fmt.Printf("AWS error: %s\n", rdserr)
+		return RCERR
+	}
+
+	// extract the reserved instance details for ec2
+	for _, ri := range ec2resp.ReservedInstances {
 
 		// compute the expiry date from start + duration
 		endDate := ri.Start.Add(time.Duration(*ri.Duration) * time.Second)
@@ -94,19 +128,8 @@ func (c *RRCommand) Run(args []string) int {
 
 	}
 
-	// Config details Keys, secret keys and region will be read from environment
-	rdssvc := rds.New(&aws.Config{MaxRetries: 10})
-
-	// Call the DescribeInstances Operation. Note Filters are not currently supported
-	rdsResp, err := rdssvc.DescribeReservedDBInstances(nil)
-
-	if err != nil {
-		fmt.Printf("Fatal error: %s\n", err)
-		return RCERR
-	}
-
-	// extract the private ip address from the instance struct stored in the reservation
-	for _, ri := range rdsResp.ReservedDBInstances {
+	// extract the rds reserved instance details for rds
+	for _, ri := range rdsresp.ReservedDBInstances {
 
 		// rds does not currently support filters so need to filter at the output end
 		if *ri.State != "active" {
@@ -136,7 +159,7 @@ func (c *RRCommand) Run(args []string) int {
 
 	}
 
-	if c.printEmpty && (len(resp.ReservedInstances)+len(rdsResp.ReservedDBInstances)) == 0 {
+	if c.printEmpty && (len(ec2resp.ReservedInstances)+len(rdsresp.ReservedDBInstances)) == 0 {
 		fmt.Printf("%s,,,,,,,,\n", c.account)
 	}
 
